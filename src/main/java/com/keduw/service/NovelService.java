2616b6ca4050b4a482c8c60aaacbc5f370f8cf02
Mr.Hong <1083178465@qq.com>
2019-05-04 20:45:00 +0800

    实现小说章节的内容编辑

---
+++ src/main/java/com/keduw/service/NovelService.java
@@ -0,0 +1,23 @@
+package com.keduw.service;
+
+import com.keduw.model.Novel;
+
+import java.util.List;
+
+public interface NovelService {
+
+    //根据类别获取信息
+    List<Novel> getNovelList(int curr, int size, int... category);
+
+    //插入小说信息
+    int insertNovel(Novel novel);
+
+    //判断小说是否存在或更新
+    int isExitOrUpdate(Novel novel);
+
+    //获取小说总数
+    int getNovelCount();
+
+    //获取小说章节总数
+    int getNovelSize(int id);
+}
