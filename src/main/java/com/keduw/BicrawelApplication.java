803fa4b26d85e9721dc908347e7d8cd66eded752
Mr.Hong <1083178465@qq.com>
2019-05-04 20:35:23 +0800

    项目创建

---
+++ src/main/java/com/keduw/BicrawelApplication.java
@@ -0,0 +1,13 @@
+package com.keduw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicrawelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicrawelApplication.class, args);
+	}
+
+}
