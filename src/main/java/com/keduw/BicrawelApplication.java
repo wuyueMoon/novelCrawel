b8c61285855d6987b058769d18debde58a082ffd
Mr.Hong <1083178465@qq.com>
2019-05-04 20:43:34 +0800

    添加配置

--- src/main/java/com/keduw/BicrawelApplication.java
+++ src/main/java/com/keduw/BicrawelApplication.java
@@ -1,9 +1,15 @@
 package com.keduw;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableScheduling
+@MapperScan("com.keduw.dao")
+@EnableTransactionManagement
 public class BicrawelApplication {
 
 	public static void main(String[] args) {
